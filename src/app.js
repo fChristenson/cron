3e28aa16ccd4f055c0dcadde9da48318289f39d1
Fredrik <fredrik.christenson@gmail.com>
2018-12-07 22:21:59 +0100

    monday job

--- src/app.js
+++ src/app.js
@@ -14,11 +14,27 @@ app.get("/outlook", async (req, res) => {
   const message = `Names available mathiasjohansson: ${isAvailable1} mathias.johansson: ${isAvailable2} is available`;
   console.log(message);
   console.log("--------------------------");
+
+  const isMonday = new Date().getDay() === 1;
+
   if (isAvailable1 || isAvailable2) {
     await mailgunService.sendEmail(
       "johansson.mathias@outlook.com",
       "Name is available",
       message
+    );
+  } else if (isMonday) {
+    console.log("Sending monday email");
+    console.log("--------------------------");
+    await mailgunService.sendEmail(
+      "fredrik.christenson@gmail.com",
+      "The name check is still running",
+      "The check for the outlook names mathiasjohansson & mathias.johansson is still running"
+    );
+    await mailgunService.sendEmail(
+      "johansson.mathias@outlook.com",
+      "The name check is still running",
+      "The check for the outlook names mathiasjohansson & mathias.johansson is still running"
     );
   }
   res.end();
