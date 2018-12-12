8361a2722debcce523c1d125780e4fd70b639d21
Fredrik <fredrik.christenson@gmail.com>
2018-12-12 19:11:56 +0100

    update

--- src/lib/services/outlookService/OutlookService.js
+++ src/lib/services/outlookService/OutlookService.js
@@ -7,12 +7,22 @@ class OutlookService {
   }
 
   async accountNameIsAvailable(name) {
-    const browser = await puppeteer.launch();
+    console.log("accountNameIsAvailable");
+    console.log("--------------------------");
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"]
+    });
     const page = await browser.newPage();
     await page.goto("https://signup.live.com/signup");
+    console.log("Visitied: https://signup.live.com/signup");
+    console.log("--------------------------");
     const selector = "#MemberName";
     await page.type(selector, `${name}@outlook.com`);
+    console.log(`Typed: ${name}`);
+    console.log("--------------------------");
     await page.click("#iSignupAction");
+    console.log("Clicked submit");
+    console.log("--------------------------");
     const response = await page.waitForResponse(req =>
       req.url().includes("CheckAvailableSigninNames")
     );
